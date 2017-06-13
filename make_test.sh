209ee7169a782aa98c3fb7de7112b5bf14403ff3
Étienne Deparis <etienne@depar.is>
2017-06-13 23:12:11 +0000

    Add an helper script to run test

---
+++ make_test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+export PATH="$PATH:$HOME/.rvm/bin"
+source "$HOME/.rvm/scripts/rvm"
+
+neruda_gem=$(realpath $1)
+
+cd
+rm -rf nerudatest
+mkdir nerudatest
+cd nerudatest
+rvm use ruby-2.4.1
+rvm --force gemset delete nerudatest
+rvm gemset create nerudatest
+rvm gemset use nerudatest
+gem install $neruda_gem
+pwd
