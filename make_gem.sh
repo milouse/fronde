29a9eb9d71fe49f50ecff1c8fc714274d6403a8c
Étienne Deparis <etienne@depar.is>
2017-08-30 20:07:20 +0000

    Fix error exit code displayed when no arg is given to ./make_gem.sh

--- make_gem.sh
+++ make_gem.sh
@@ -4,3 +4,5 @@ pandoc -t markdown_github -o README.md README.org
 GEMFILE=$(gem build neruda.gemspec | sed -n 's/.*File: \(.*\)$/\1/p')
 echo "Successfully generated $GEMFILE"
 [ "z$1" = ztest ] && ./make_test.sh "$GEMFILE"
+# Remove unwanted error code when no args is given
+exit 0
