b3a01499d9d0d95426b07a885c9f16066804f551
Étienne Deparis <etienne@depar.is>
2019-06-24 20:39:16 +0000

    Do not use default templates

--- lib/neruda/config.rb
+++ lib/neruda/config.rb
@@ -48,48 +48,6 @@ module Neruda
         @config['public_folder'] ||= 'public_html'
         @config['blog_path'] ||= 'blog'
         @config['templates'] ||= []
-        license = Neruda::Config.settings['license']
-        license_meta = ''
-        if license
-          license = license.split("\n").map(&:strip).join(' ')
-          license_meta = "<meta property=\"dc.rights\" content=\"#{license}\">"
-        end
-        twitter_handle = Neruda::Config.settings['twitter_account']
-        twitter_meta = ''
-        if twitter_handle
-          twitter_meta = <<~TWITTER
-            <meta name="twitter:creator" content="@#{twitter_handle}">
-          TWITTER
-        end
-        title = Neruda::Config.settings['title'] || ''
-        title = title.split("\n").map(&:strip).join(' ')
-        metadata = <<~ENDMETA
-          <meta name="description" lang="%l" content="TBD">
-          <link rel="schema.dc" href="http://purl.org/dc/elements/1.1/">
-          <meta property="dc.publisher" content="%a">
-          <meta property="dc.type" content="text">
-          <meta property="dc.format" content="text/html">
-          <meta property="dc.title" lang="%l" content="%t">
-          <meta property="dc.description" lang="%l" content="TBD">
-          <meta property="dc.language" content="%l">
-          <meta property="dc.date" content="%I">
-          #{license_meta}
-
-          <meta name="twitter:card" content="summary">
-          #{twitter_meta}
-          <meta property="og:type" content="article">
-          <meta property="og:title" content="%t">
-          <meta property="og:article:published_time" content="%I">
-          <meta property="og:url" content="%u">
-          <meta property="og:locale" content="%l">
-          <meta property="og:description" content="TBD">
-          <meta property="og:site_name" content="#{title}">
-        ENDMETA
-        @config['templates'] << {
-          'type' => 'before',
-          'selector' => 'title',
-          'content' => metadata
-        }
       end
     end
   end
