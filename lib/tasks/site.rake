milouse/fronde

commit 830e81fd59205ddfa6835d809bfe35df6014aef4

Fix error handling in Emacs publish threads

lib/tasks/site.rake
@@ -28,7 +28,12 @@ namespace :site do
     build_html = Thread.new do
       Neruda::OrgFile.new(nil, verbose: Rake::FileUtilsExt.verbose_flag).publish
     end
-    Neruda::Utils.throbber(build_html, 'Publishing:')
+    begin
+      Neruda::Utils.throbber(build_html, 'Publishing:')
+    rescue RuntimeError
+      warn 'Aborting'
+      next
+    end
     customize_html = Thread.new do
       pubfolder = Neruda::Config.settings['public_folder']
       Dir["#{pubfolder}/**/*.html"].each do |f|
@@ -46,8 +51,15 @@ namespace :site do
         next
       end
       verbose = Rake::FileUtilsExt.verbose_flag
-      o = Neruda::OrgFile.new(args[:source], verbose: verbose)
-      o.publish
+      o = Thread.new do
+        Neruda::OrgFile.new(args[:source], verbose: verbose).publish
+      end
+      begin
+        Neruda::Utils.throbber(o, 'Publishing:')
+      rescue RuntimeError
+        warn 'Aborting'
+        next
+      end
       target = Neruda::OrgFile.target_for_source(args[:source])
       warn "Customizing file #{target}" if verbose
       Neruda::Templater.customize_output(target, o)

lib/neruda/org_file/htmlizer.rb
@@ -70,9 +70,9 @@ module Neruda
       command = emacs_command arguments
       if @options[:verbose]
         warn command
-        return system(command)
+        return system(command, exception: true)
       end
-      system command, out: '/dev/null', err: '/dev/null'
+      system command, out: '/dev/null', err: '/dev/null', exception: true
     end
   end
 end

lib/neruda/utils.rb
@@ -71,17 +71,19 @@ module Neruda
       # @param message [String] the message to display before the throbber
       # @return [void]
       def throbber(thread, message)
-        model = Neruda::Config.settings['throbber'] || 'default'
-        model = 'default' unless Neruda::Utils::THROBBER_FRAMES.has_key?(model)
-        frames = Neruda::Utils::THROBBER_FRAMES[model]
-        current = 0
-        while thread.alive?
-          sleep 0.1
-          print "#{message} #{frames[current % frames.length]}\r"
-          current += 1
+        frames = select_throbber_frames
+        begin
+          run_and_decorate_thread thread, message, frames
+        rescue RuntimeError => e
+          done = Rainbow('An error occured.').bold.red + "\n"
+          done += Rainbow('To see it, run again your command with more ' \
+                          'verbosity, i.e. pablo build -v').bold
+          warn "#{message} #{done}"
+          raise e
+        else
+          done = Rainbow('done'.ljust(frames[0].length)).green
+          puts "#{message} #{done}"
         end
-        done = Rainbow('done'.ljust(frames[0].length)).green
-        puts "#{message} #{done}"
       end
 
       # Returns the short and long options specification for a given
@@ -152,6 +154,24 @@ module Neruda
         end
         return 'apple' if RUBY_PLATFORM =~ /darwin/
         'linux'
+      end
+
+      private
+
+      def select_throbber_frames
+        model = Neruda::Config.settings['throbber'] || 'default'
+        model = 'default' unless Neruda::Utils::THROBBER_FRAMES.has_key?(model)
+        Neruda::Utils::THROBBER_FRAMES[model]
+      end
+
+      def run_and_decorate_thread(thread, message, frames)
+        thread.abort_on_exception = true
+        current = 0
+        while thread.alive?
+          sleep 0.1
+          print "#{message} #{frames[current % frames.length]}\r"
+          current += 1
+        end
       end
     end
   end

commit d8c59f6d42d32d3e1cdccce394149899d7698716

fix: Remove crash due to a wild variable name reuse
Bug was introduced in 830e81f

lib/tasks/site.rake
@@ -51,18 +51,20 @@ namespace :site do
         next
       end
       verbose = Rake::FileUtilsExt.verbose_flag
-      o = Thread.new do
-        Neruda::OrgFile.new(args[:source], verbose: verbose).publish
+      build_html = Thread.new do
+        o = Neruda::OrgFile.new(args[:source], verbose: verbose)
+        Thread.current[:org_file] = o
+        o.publish
       end
       begin
-        Neruda::Utils.throbber(o, 'Building:')
+        Neruda::Utils.throbber(build_html, 'Building:')
       rescue RuntimeError
         warn 'Aborting'
         next
       end
       target = Neruda::OrgFile.target_for_source(args[:source])
       warn "Customizing file #{target}" if verbose
-      Neruda::Templater.customize_output(target, o)
+      Neruda::Templater.customize_output(target, build_html[:org_file])
     end
   end
 