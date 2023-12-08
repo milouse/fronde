70407e067a91cb5538af78ae8aaa09ab8cd83c0a
Étienne Deparis <etienne@depar.is>
2023-12-08 20:51:46 +0100

    fix: Avoid a crash in tests when run with various org-mode version

--- spec/rake_preview_spec.rb
+++ spec/rake_preview_spec.rb
@@ -6,6 +6,8 @@ require 'net/http'
 
 def fetch_test_content(path)
   page_content = URI("http://localhost:5000/#{path}").open.read
+  # Ensure tests always pass, whatever charset is generated
+  page_content.gsub!('charset=UTF-8', 'charset=utf-8')
   page_content.gsub(
     /[FMSTW][a-z]+ \d{1,2} of [ADFJMNOS][a-z]+, \d{4} at \d{2}:\d{2}/,
     '__PUB_DATE__'
